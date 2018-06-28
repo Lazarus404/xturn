32be7559a9cd79063901941365ff7ce35541a0ec
Lazarus404 <lee.sylvester@gmail.com>
2018-06-28 19:56:01 +0100

    Add debug

--- apps/xturn/lib/xturn/parse.ex
+++ apps/xturn/lib/xturn/parse.ex
@@ -356,11 +356,12 @@ defmodule Xirsys.Turn.Parse do
     case Store.lookup(tuple5) do
       {:ok, [client, {_relay_ip, _relay_port}, _, _]} ->
         Logger.debug "Refreshing with 0 time"
-        AllocateClient.refresh(client, 600)
+        AllocateClient.refresh(client, 0)
         conn
       {:error, :not_found} ->
         Conn.halt(conn)
     end
+    IO.puts "COULD NOT FIND ALLOCATION #{inspect tuple5}"
     Conn.response(conn, 437, "Allocation Mismatch")
   end
   defp do_refresh(conn, <<b::32>>, tuple5) when is_integer(b) do
