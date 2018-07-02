13f23a4ba933da2962ad1fbb4e9e3d37601bc7f3
Lazarus404 <lee.sylvester@gmail.com>
2018-07-02 20:22:26 +0100

    Add distillery

--- mix.exs
+++ mix.exs
@@ -11,7 +11,8 @@ defmodule XirsysTurnServer.Mixfile do
   defp deps() do
     [
       {:dialyze, ">= 0.1.4"},
-      {:ex_doc,  "~> 0.18.3"}
+      {:ex_doc,  "~> 0.18.3"},
+      {:distillery, "~> 1.5", runtime: false}
     ]
   end
 end
