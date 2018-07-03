a06efca3e495c6351c38502d611882615d6c9072
Lazarus404 <lee.sylvester@gmail.com>
2018-07-03 20:36:50 +0100

    Cleanup

--- apps/xturn/lib/utils/socket.ex
+++ apps/xturn/lib/utils/socket.ex
@@ -64,6 +64,7 @@ defmodule Xirsys.Utils.Socket do
   @doc """
   Opens a new port for UDP TURN transport
   """
+  @spec open_turn_port(tuple(), atom(), list()) :: {:ok, Socket.t} | {:error, atom()}
   def open_turn_port({_, _, _, _} = sip, policy, opts) do
     udp_options = [{:ip, sip}, {:active, :once}, {:buffer, 1024*1024*1024}, {:recbuf, 1024*1024*1024}, {:sndbuf, 1024*1024*1024}, :binary] ++ opts #[{:buffer, 1024*1024*1024}, {:recbuf, 1024*1024*1024}, {:sndbuf, 1024*1024*1024}, {:exit_on_close, true}, {:keepalive, true}, {:nodelay, true}, {:packet, :raw}]
     open_free_udp_port(policy, udp_options)
@@ -72,123 +73,18 @@ defmodule Xirsys.Utils.Socket do
   @doc """
   With new data, see if we can process a message on the buffer
   """
+  @spec process_buffer(binary(), binary(), tuple(), function()) :: binary()
   def process_buffer(data, buffer, addr, callback) do
     bin = <<buffer::binary, data::binary>>
     process_buffer(bin, addr, callback)
   end
 
-  defp process_buffer(bin, _, _) when byte_size(bin) <= 4, do: bin
-  defp process_buffer(<<type::2, _::14, body_bytes::16, _body::binary-size(body_bytes), _::binary>> = bin, addr, callback) when type == @send_msg
-                                                                                                                             or type == @channel_msg do
-    msg_bytes = pad_body_bytes(type, body_bytes)
-    process_data(bin, msg_bytes, addr, callback)
-  end
-  defp process_buffer(<<type::2, _::14, _body_bytes::16, _::binary>> = bin, _, _) when type == @send_msg or type == @channel_msg,
-    do: bin # message is not yet long enough
-  defp process_buffer(<<type::2, _::14, _::binary>> = bin, _, _) do
-    Logger.error "Unknown message type : #{inspect type}"
-    bin
-  end
-
   @doc """
-  With a packet header extracted from the buffer,  see
-  if we can process it
+  Performs the SSL/TLS/DTLS server-side handshake if a secure
+  socket, otherwise simply accepts an incoming connection request
+  on a listening socket.
   """
-  defp process_data(bin, required_size, _, _) when required_size > byte_size(bin), do: bin # need more data
-  defp process_data(bin, required_size, {{cip, cport}, {sip, sport}}, callback) when required_size == byte_size(bin) do
-    process_msg(callback, bin, {self(), cip, cport, sip, sport})
-    <<>>
-  end
-  defp process_data(bin, required_size, {{cip, cport}, {sip, sport}} = addr, callback) do
-    # parse TURN message
-    <<turn::binary-size(required_size), tail::binary>> = bin
-    Logger.debug "Tail is: #{inspect tail}"
-    #ns = process_turn_msg(turn, %{state | turn_msg_buffer: bin})
-    process_msg(callback, turn, {self(), cip, cport, sip, sport})
-    process_buffer(tail, addr, callback)
-  end
-
-  defp pad_body_bytes(@channel_msg, bytes),
-    do: roundup_to_4(bytes)
-  defp pad_body_bytes(@send_msg, bytes),
-    do: bytes + 20
-
-  @doc """
-  Pad message if needed
-  """
-  defp roundup_to_4(num) do
-    pad = rem(num, 4)
-    padded = num+(4-pad)
-    if rem(num, 4) == 0, do: padded, else: padded + 4
-  end
-
-  @doc """
-  Calls the callback handler
-  """
-  defp process_msg(cb, msg, {listener, fip, fport, tip, tport}) do
-    apply cb, :process_message, [%Conn{
-        message: msg,
-        listener: listener,
-        client_ip: fip,
-        client_port: fport,
-        server_ip: tip,
-        server_port: tport
-      }]
-  end
-
-  @doc """
-  Opens an available UDP port as per requirement.
-  See RFC's
-  """
-  defp open_free_udp_port(:random, udp_options) do
-    ## BUGBUG: Should be a random port
-    case :gen_udp.open(0, udp_options) do
-      {:ok, socket} ->
-        {:ok, %Socket{type: :udp, sock: socket}}
-      {:error, reason} ->
-        Logger.error "UDP open #{inspect udp_options} -> #{inspect reason}"
-        {:error, reason}
-      {EXIT, _} = reason ->
-        Logger.error "UDP open #{inspect udp_options} -> #{inspect reason}"
-        {:error, reason}
-    end
-  end
-  defp open_free_udp_port({:range, min_port, max_port}, udp_options) when min_port <= max_port do
-    case :gen_udp.open(min_port, udp_options) do
-      {:ok, socket} ->
-        {:ok, %Socket{type: :udp, sock: socket}}
-      {:error, :eaddrinuse} ->
-        policy2 = {:range, min_port + 1, max_port}
-        open_free_udp_port(policy2, udp_options)
-      {:error, reason} ->
-        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
-        {:error, reason}
-      {EXIT, _} = reason ->
-        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
-        {:error, reason}
-    end
-  end
-  defp open_free_udp_port({:range, _min_port, _max_port}, udp_options) do
-    reason = "Port range exhausted"
-    Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
-    {:error, reason}
-  end
-  defp open_free_udp_port({:preferred, port}, udp_options) do
-    case :gen_udp.open(port, udp_options) do
-      {:ok, socket} ->
-        {:ok, %Socket{type: :udp, sock: socket}}
-      {:error, :eaddrinuse} ->
-        policy2 = :random
-        open_free_udp_port(policy2, udp_options)
-      {:error, reason} ->
-        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
-        {:error, reason}
-      {EXIT, _} = reason ->
-        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
-        {:error, reason}
-    end
-  end
-
+  @spec handshake(Socket.t) :: {:ok, Socket.t}
   def handshake(%Socket{type: :tcp, sock: socket}) do
     with {:ok, cli_socket} <- :gen_tcp.accept(socket) do
       {:ok, %Socket{type: :tcp, sock: cli_socket}}
@@ -314,4 +210,112 @@ defmodule Xirsys.Utils.Socket do
   end
   def close(nil, _),
     do: Logger.debug "Caught attempted close of nil socket"
+
+  # ----------------------------
+  # Private functions
+  # ----------------------------
+
+  defp process_buffer(bin, _, _) when byte_size(bin) <= 4, do: bin
+  defp process_buffer(<<type::2, _::14, body_bytes::16, _body::binary-size(body_bytes), _::binary>> = bin, addr, callback) when type == @send_msg
+                                                                                                                             or type == @channel_msg do
+    msg_bytes = pad_body_bytes(type, body_bytes)
+    process_data(bin, msg_bytes, addr, callback)
+  end
+  defp process_buffer(<<type::2, _::14, _body_bytes::16, _::binary>> = bin, _, _) when type == @send_msg or type == @channel_msg,
+    do: bin # message is not yet long enough
+  defp process_buffer(<<type::2, _::14, _::binary>> = bin, _, _) do
+    Logger.error "Unknown message type : #{inspect type}"
+    bin
+  end
+
+  # With a packet header extracted from the buffer,  see
+  # if we can process it
+  defp process_data(bin, required_size, _, _) when required_size > byte_size(bin), do: bin # need more data
+  defp process_data(bin, required_size, {{cip, cport}, {sip, sport}}, callback) when required_size == byte_size(bin) do
+    process_msg(callback, bin, {self(), cip, cport, sip, sport})
+    <<>>
+  end
+  defp process_data(bin, required_size, {{cip, cport}, {sip, sport}} = addr, callback) do
+    # parse TURN message
+    <<turn::binary-size(required_size), tail::binary>> = bin
+    Logger.debug "Tail is: #{inspect tail}"
+    #ns = process_turn_msg(turn, %{state | turn_msg_buffer: bin})
+    process_msg(callback, turn, {self(), cip, cport, sip, sport})
+    process_buffer(tail, addr, callback)
+  end
+
+  defp pad_body_bytes(@channel_msg, bytes),
+    do: roundup_to_4(bytes)
+  defp pad_body_bytes(@send_msg, bytes),
+    do: bytes + 20
+
+  # Pad message if needed
+  defp roundup_to_4(num) do
+    pad = rem(num, 4)
+    padded = num+(4-pad)
+    if rem(num, 4) == 0, do: padded, else: padded + 4
+  end
+
+  # Calls the callback handler
+  defp process_msg(cb, msg, {listener, fip, fport, tip, tport}) do
+    apply cb, :process_message, [%Conn{
+        message: msg,
+        listener: listener,
+        client_ip: fip,
+        client_port: fport,
+        server_ip: tip,
+        server_port: tport
+      }]
+  end
+
+  # Opens an available UDP port as per requirement.
+  # See RFC's
+  defp open_free_udp_port(:random, udp_options) do
+    ## BUGBUG: Should be a random port
+    case :gen_udp.open(0, udp_options) do
+      {:ok, socket} ->
+        {:ok, %Socket{type: :udp, sock: socket}}
+      {:error, reason} ->
+        Logger.error "UDP open #{inspect udp_options} -> #{inspect reason}"
+        {:error, reason}
+      {EXIT, _} = reason ->
+        Logger.error "UDP open #{inspect udp_options} -> #{inspect reason}"
+        {:error, reason}
+    end
+  end
+  defp open_free_udp_port({:range, min_port, max_port}, udp_options) when min_port <= max_port do
+    case :gen_udp.open(min_port, udp_options) do
+      {:ok, socket} ->
+        {:ok, %Socket{type: :udp, sock: socket}}
+      {:error, :eaddrinuse} ->
+        policy2 = {:range, min_port + 1, max_port}
+        open_free_udp_port(policy2, udp_options)
+      {:error, reason} ->
+        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
+        {:error, reason}
+      {EXIT, _} = reason ->
+        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
+        {:error, reason}
+    end
+  end
+  defp open_free_udp_port({:range, _min_port, _max_port}, udp_options) do
+    reason = "Port range exhausted"
+    Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
+    {:error, reason}
+  end
+  defp open_free_udp_port({:preferred, port}, udp_options) do
+    case :gen_udp.open(port, udp_options) do
+      {:ok, socket} ->
+        {:ok, %Socket{type: :udp, sock: socket}}
+      {:error, :eaddrinuse} ->
+        policy2 = :random
+        open_free_udp_port(policy2, udp_options)
+      {:error, reason} ->
+        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
+        {:error, reason}
+      {EXIT, _} = reason ->
+        Logger.error "UDP open #{inspect [0 | udp_options]} -> #{inspect reason}"
+        {:error, reason}
+    end
+  end
 end
